9cb9cc54d3e9d4e7aac66a05d73f163dc20c27d7
ahmedmuhammed <ahmedmuhammedem@gmail.com>
2024-02-04 22:36:22 +0300

    feat: Implement Easybank Landing Page with Tailwind CSS
    - Configured Tailwind CSS for styling
    - Added hero section to the landing page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    colors: {
+      white: "#ffffff",
+      "dark-blue": "#2d314d",
+      "lime-green": "#31d35c",
+      "bright-cyan": "#2bb7da",
+      "gray-blue": "#9698a6",
+      "light-gray-blue": "#f3f4f6",
+      "light-gray": "#fafafa",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
